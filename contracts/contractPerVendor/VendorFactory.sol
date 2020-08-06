aa1d90c09481aa4eb734c4bffddcc46a22076363
0Alic <andrealisi.12lj@gmail.com>
2020-08-06 18:33:06 +0200

    Added Factory contract to avoid bytecode too large

--- contracts/contractPerVendor/VendorFactory.sol
+++ contracts/contractPerVendor/VendorFactory.sol
@@ -5,18 +5,31 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VendorFactory is Ownable {
 
+    bool public working = false;
     mapping(address => bool) public contracts;
 
-    constructor(address _authorityContract) public {
+    function createVendorContract(address _vendor) onlyOwner public virtual returns(VendorContract) {
 
-        transferOwnership(_authorityContract); 
-    }
-
-    function createVendorContract(address _vendor) onlyOwner public returns(VendorContract) {
+        require(working == true, "Factory is not working");
 
         VendorContract c = new VendorContract(_vendor, msg.sender);
         contracts[address(c)] = true;
 
         return c;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public virtual override {
+        super.transferOwnership(newOwner);
+        working = true;        
+    }
+}
+
+contract VendorFactoryTest is VendorFactory {
+
+    event ContractAddress(address c);
+
+    function createVendorContract(address _vendor) onlyOwner public virtual override returns(VendorContract) {
+        VendorContract c = super.createVendorContract(_vendor);
+        emit ContractAddress(address(c));
     }
 }
