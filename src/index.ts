7383fb2697c67fd9998a5a8f67ae059a4e73c859
Prateeti98 <mukherjeeprateeti01@gmail.com>
2020-09-30 06:26:35 +0300

    add source files

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { VulnerabilityDetailsContract } from './vulnerability-details-contract';
+export { VulnerabilityDetailsContract } from './vulnerability-details-contract';
+
+export const contracts: any[] = [ VulnerabilityDetailsContract ];
