d1544f2d700063574e2893325beeb735e300d115
Prateeti98 <mukherjeeprateeti01@gmail.com>
2020-11-16 07:21:32 +0200

    remove outdated file

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { VulnerabilityDetailsContract } from './vulnerability-details-contract';
-export { VulnerabilityDetailsContract } from './vulnerability-details-contract';
-
-export const contracts: any[] = [ VulnerabilityDetailsContract ];
