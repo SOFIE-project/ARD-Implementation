358a650a2681e8516a27fab9a19b39855b8dfa01
ldesantis123 <63883615+ldesantis123@users.noreply.github.com>
2020-12-07 23:05:36 +0100

    Added IntParser/ Changed Reciver

--- ARD public ledger/contracts/contractPerVendor/AuthorityContract.sol
+++ ARD public ledger/contracts/contractPerVendor/AuthorityContract.sol
@@ -357,8 +357,11 @@ contract AuthorityContract is Ownable, InterledgerSenderInterface, InterledgerRe
         // Decoding
         // Get the Id from the data field
         uint id;
+        string id_string
         string memory location;
-        (id, location) = abi.decode(data, (uint256, string));
+        (id_string, location) = abi.decode(data, (string, string));
+        
+        id=parseInt(id_string);
 
         // Retrive VendorContract
         require(haveVulnerability(id), "vulnerabilityId does not exist");
@@ -436,6 +439,35 @@ contract AuthorityContract is Ownable, InterledgerSenderInterface, InterledgerRe
 
         (,VendorContract.State _state,,uint _timelock,uint _acktimelock,,)=vendorContract.getVulnerabilityInfo(_vulnerabilityId);
         return  ((_state == VendorContract.State.Valid && _acktimelock < block.timestamp ) || (_state == VendorContract.State.Acknowledged && _timelock < block.timestamp));
+    }
+    
+    \\utility funtion for parsing
+    
+    function parseInt(string memory _a) public pure returns (int) {
+       return parseInt(_a, 0);
+    }
+
+    function parseInt(string memory a, uint b) public pure returns (int) {
+       bytes memory bresult = bytes(_a);
+       int mint = 0;
+       bool decimals = false;
+       bool negative = false;
+       for (uint i=0; i<bresult.length; i++){
+          if ((i == 0) && (bresult[i] == '-')) {
+             negative = true;
+          }
+          if ((uint8(bresult[i]) >= 48) && (uint8(bresult[i]) <= 57)) {
+            if (decimals){
+              if (_b == 0) break;
+              else _b--;
+             }
+          mint *= 10;
+          mint += uint8(bresult[i]) - 48;
+          } else if (uint8(bresult[i]) == 46) decimals = true;
+       }
+       if (_b > 0) mint *= int(10**_b);
+       if (negative) mint *= -1;
+       return mint;
     }
 
 }
